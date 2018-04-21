7af59757c3f35dce60941418ffe6fe5c12ce285b
SihakChau <chau.sihak@puc.edu.kh>
2018-04-21 17:02:04 +0700

    70% home ui

---
+++ app/component/subCarousel/styles/sliderEntry.js
@@ -0,0 +1,57 @@
+import { StyleSheet, Dimensions, Platform } from 'react-native';
+import { colors } from './index';
+
+const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
+
+function wp (percentage) {
+    const value = (percentage * viewportWidth) / 100;
+    return Math.round(value);
+}
+
+const slideHeight = 80;
+const slideWidth = 120;
+const itemHorizontalMargin = wp(1);
+
+export const sliderWidth = viewportWidth;
+export const itemWidth = slideWidth + itemHorizontalMargin * .5;
+
+const entryBorderRadius = 0;
+
+export default StyleSheet.create({
+    slideInnerContainer: {
+        width: itemWidth,
+        height: slideHeight,
+        paddingHorizontal: itemHorizontalMargin,
+    },
+    imageContainer: {
+        flex: 1,
+        backgroundColor: colors.slideBackground,
+        borderTopLeftRadius: 4,
+        borderTopRightRadius: 4,
+        borderBottomLeftRadius: 4,
+        borderBottomRightRadius: 4
+    },
+    imageContainerEven: {
+        backgroundColor: colors.slideBackground
+    },
+    image: {
+        ...StyleSheet.absoluteFillObject,
+        resizeMode: 'cover',
+        borderRadius: Platform.OS === 'ios' ? entryBorderRadius : 0,
+        borderTopLeftRadius: entryBorderRadius,
+        borderTopRightRadius: entryBorderRadius
+    },
+    // image's border radius is buggy on ios; let's hack it!
+    radiusMask: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: entryBorderRadius,
+        backgroundColor: colors.slideBackground
+    },
+    radiusMaskEven: {
+        backgroundColor: colors.slideBackground
+    },
+
+});
--- app/screen/home/index.js
+++ app/screen/home/index.js
@@ -1,27 +1,89 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, Dimensions, Text, SafeAreaView, Platform } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MainCarousel from '../../component/carousel'
+import SubCarousel from '../../component/subCarousel'
 
-// create a component
+import { COLORS } from '../../module'
+const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
+import Carousel from 'react-native-snap-carousel';
 class HomeApp extends Component {
     render() {
+        const data = [
+            {
+                title: 'Beautiful and dramatic Antelope Canyon',
+                subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+                illustration: 'https://i.imgur.com/UYiroysl.jpg'
+            },
+            {
+                title: 'Earlier this morning, NYC',
+                subtitle: 'Lorem ipsum dolor sit amet',
+                illustration: 'https://i.imgur.com/UPrs1EWl.jpg'
+            },
+            {
+                title: 'White Pocket Sunset',
+                subtitle: 'Lorem ipsum dolor sit amet et nuncat ',
+                illustration: 'https://i.imgur.com/MABUbpDl.jpg'
+            },
+            {
+                title: 'Acrocorinth, Greece',
+                subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+                illustration: 'https://i.imgur.com/KZsmUi2l.jpg'
+            },
+            {
+                title: 'The lone tree, majestic landscape of New Zealand',
+                subtitle: 'Lorem ipsum dolor sit amet',
+                illustration: 'https://i.imgur.com/2nCt3Sbl.jpg'
+            },
+            {
+                title: 'Middle Earth, Germany',
+                subtitle: 'Lorem ipsum dolor sit amet',
+                illustration: 'https://i.imgur.com/lceHsT6l.jpg'
+            }
+        ];
         return (
-            <View style={styles.container}>
-                <Text>HomeApp</Text>
-            </View>
+            <SafeAreaView style={styles.container}>
+                <View style = {{ flexDirection: 'row', justifyContent: 'space-between', alignItems : 'center'}}>
+                <Text style={styles.subHearderTittle}> Recommend </Text>
+                <FontAwesome style = {{ fontSize: 28, marginRight: 12, color : COLORS.SUB_HEADER_TITTLE, fontWeight: '300' }} name= 'search'  />
+                </View>
+                <View style={styles.carouselContainer} >
+                    <MainCarousel data={data} />
+                </View>
+                <Text style={[styles.subHearderTittle, {fontSize:22, marginTop:15 }]}> Services </Text>
+                <View style={styles.SubCarouselContainer}>
+                    <SubCarousel data={data} />
+                </View>
+                <View style={styles.gridMenu} ></View>
+            </SafeAreaView>
         );
     }
 }
 
-// define your styles
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+    carouselContainer: {
+        height:210 ,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#2c3e50',
     },
+    SubCarouselContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 100,
+    },
+    subHearderTittle: {
+        fontSize: 32,
+        fontWeight: '600',
+        color: COLORS.SUB_HEADER_TITTLE
+    },
+    container: {
+        margin: 10,
+        flex: 1,
+    },
+
+    profileUser: {
+    }
 });
 
-//make this component available to the app
 export default HomeApp;
